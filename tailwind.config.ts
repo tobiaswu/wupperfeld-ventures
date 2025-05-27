26785d0b55ca855a68742df024db4bcf2588ee42
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2025-05-27 12:13:46 -0500

    chore: upgrade tailwind

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-};
-export default config;
