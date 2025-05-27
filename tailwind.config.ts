5b82b2ce9adbb5f14ca15f42e52130bd0c02a931
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2025-05-27 12:11:25 -0500

    chore: updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,19 +5,5 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#ECBA82',
-          secondary: '#4F7CAC',
-          accent: '#A24936',
-          neutral: '#22333B',
-          'base-100': '#272727',
-        },
-      },
-    ],
-  },
-  plugins: [require('daisyui')],
 };
 export default config;
